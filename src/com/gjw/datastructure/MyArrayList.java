dd7dfc9aa2545c401f9795001367b8bd759861dd
gjw <544892650@qq.com>
2019-04-22 15:24:25 +0800

    guojiangwei initial

---
+++ src/com/gjw/datastructure/MyArrayList.java
@@ -0,0 +1,21 @@
+package com.gjw.datastructure;
+
+import java.util.Iterator;
+
+public class MyArrayList<E> implements Iterator<E> {
+
+	@Override
+	public boolean hasNext() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public E next() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+
+}
