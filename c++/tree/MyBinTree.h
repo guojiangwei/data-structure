599fe9ee53cbd56fe55157b4d119b60f533f3e66
guojiangwei <544892650@qq.com>
2019-08-01 17:29:57 +0800

    coding bintree

--- c++/tree/MyBinTree.h
+++ c++/tree/MyBinTree.h
@@ -1,18 +1,102 @@
 #include "MyBinTreeNode.h"
-class MyBinTree
+template <typename T> class MyBinTree
 {
-private:
-    /* data */
+protected:
+///规模和根节点
+    int _size;MyBinTreeNodePosi(T) _root;
+    ////更新节点高度
+    virtual int updateHeight(MyBinTreeNodePosi(T) x);
+    //更新节点及其祖先的高度
+    void updateHeightAbove(MyBinTreeNodePosi(T) x);
 public:
-    MyBinTree(/* args */);
-    ~MyBinTree();
+    MyBinTree(/* args */):_size(0),_root(NULL){}
+    ~MyBinTree(){
+        if(0<_size )remove(_root);
+    }
+    int size(){return _size;}
+    bool empty(){return !_root;}
+    MyBinTreeNodePosi(T) root(){return _root;}
+    ///插入根节点
+    MyBinTreeNodePosi(T) insertAsRoot(T const & e);
+    //将e作为x的左孩子插入
+    MyBinTreeNodePosi(T) insertAsLChild(MyBinTreeNodePosi(T) x,T const & e);
+    //将e作为x的右孩子插入
+    MyBinTreeNodePosi(T) insertAsRChild(MyBinTreeNodePosi(T) x,T const & e);
+    //将t作为x的左孩子接入
+    MyBinTreeNodePosi(T) attachAsLChild(MyBinTreeNodePosi(T) x,MyBinTree<T> * &t);
+    //将t作为x的右孩子接入
+    MyBinTreeNodePosi(T) attachAsRChild(MyBinTreeNodePosi(T) x,MyBinTree<T> * &t);
+    //删除以节点x为根的子树，并返回移除的子树的规模
+    int remove(MyBinTreeNodePosi(T) x);
+    //将子树x从当前树种移除作为一个单独的树
+    MyBinTree<T> * secede(MyBinTreeNodePosi(T) x);
+    ////遍历
+    template <typename VST> void travLevel(VST& visit){ if (_root) _root->travLevel(visit); }
+    template <typename VST> void travPre(VST& visit){if (_root) _root->travPre(visit);}
+    template <typename VST> void travIn(VST& visit){ if (_root)  _root->travIn(visit); }  
+    template <typename VST> void travPost(VST& visit){if (_root)   _root->travPost(visit);}
+
+    ///比较器
+    bool operator<(MyBinTree<T> const & t){
+        return _root && t._root && lt(_root,t._root);
+    }
+
+    bool operator== (MyBinTree<T> const & t){
+        return _root && t._root && (_root==t._root);
+    }
 };
 
-MyBinTree::MyBinTree(/* args */)
-{
+///更新高度
+template <typename T> int MyBinTree<T>::updateHeight(MyBinTreeNodePosi(T) x){
+    return x->height=1+max(stature(x->lc),stature(x->rc));
 }
 
-MyBinTree::~MyBinTree()
-{
+
+///更新所有祖先节点高度
+template <typename T> int MyBinTree<T>::updateHeight(MyBinTreeNodePosi(T) x){
+    while (x)
+    {
+       updateHeight(x);
+       x=x->parent;
+    }
+    
+}
+template <typename T> MyBinTreeNodePosi(T) MyBinTree<T>::insertAsRoot(T const & e){
+    _size=1;
+    return _root=new MyBinTreeNode<T>(e);
+}
+template <typename T> MyBinTreeNodePosi(T) MyBinTree<T>::insertAsLChild(MyBinTreeNodePosi(T) x,T const& e){
+    x->insertAsLc(e);
+    _size++;
+    updateHeightAbove(x);
+    return x->lc;
 }
 
+template <typename T> MyBinTreeNodePosi(T) MyBinTree<T>::insertAsRChild(MyBinTreeNodePosi(T) x,T const& e){
+    x->insertAsRc(e);
+    _size++;
+    updateHeightAbove(x);
+    return x->rc;
+}
+
+template <typename T> MyBinTreeNodePosi(T) MyBinTree<T>::attachAsLChild(MyBinTreeNodePosi(T) x,MyBinTree<T> * &t){
+    if (x->lc=t->_root) x->lc->parent=x; ///将树t的根节点接到x的左边，并更改父节点指向
+    _size=_size+t->_size;
+    updateHeightAbove(x);
+    t->_root=NULL;
+    t->_size=0;
+    release(t);
+    t=NULL;
+    return x;
+}
+
+template <typename T> MyBinTreeNodePosi(T) MyBinTree<T>::attachAsRChild(MyBinTreeNodePosi(T) x,MyBinTree<T> * &t){
+    if (x->rc=t->_root) x->rc->parent=x; ///将树t的根节点接到x的又边，并更改父节点指向
+    _size=_size+t->_size;
+    updateHeightAbove(x);
+    t->_root=NULL;
+    t->_size=0;
+    release(t);
+    t=NULL;
+    return x;
+}
--- c++/tree/MyBinTreeNode.h
+++ c++/tree/MyBinTreeNode.h
@@ -1,15 +1,70 @@
-#define MyBinTreeNode(T) MyBinTreeNodePosi<T> *
+#define MyBinTreeNodePosi(T) MyBinTreeNode<T> *
+///空树高度为-1
+#define stature(p) ((p)?(p)->height:-1)
+/********************************************
+ * 与节点状态相关的宏
+**********************************************/
+#define lsRoot(x) (!((x)->parent))
+#define lsLChild(x) (!lsRoot(x)&&(&(x)==(x)->prarent->lc))
+#define lsRChild(x) (!lsRoot(x)&&(&(x)==(x)->prarent->rc))
+#define hasParent(x) (!lsRoot(x))
+#define hasLChild(x) ((x)->lc)
+#define hasRChild(x) ((x)->rc)
+///至少有一个孩子
+#define hasChild(x) (hasLChild(x) || hasRChild(x))
+///同时有两个孩子
+#define hasBothChild(x) (hasLChild(x) && hasRChild(x))
+#define lsLeaf(x) (!hasChild(x))
+///获取兄弟节点
+#define sibling(p) (lsLChild(p)?(p)->parent->rc:(p)->parent->lc)
+//获取叔叔节点
+#define uncle(x) (lsLChild(x->parent)?(x)->parent->parent->rc:(x)->parent->parent->lc)
 typedef enum {RB_RED,RB_BLACK} RBColor;
-template <typename T> struct MyBinTreeNode //
+//二叉树节点数据结构
+template <typename T> struct MyBinTreeNode 
 {
-    //
+    //节点数据
     T data;
+    //每个二叉树节点有三个指针，分别指向父亲，左孩子，右孩子
     MyBinTreeNode(T) parent;MyBinTreeNode(T) lc;MyBinTreeNode(T) rc;
+    //节点在二叉树中的高度，方便以后实现AVL等二叉树
     int height;
-    //
+    //NULL Path Length (左式堆空路径长度，也可以用height代替)
     int npl; 
-    //R
+    //节点颜色，方便后面实现红黑树
     RBColor color;
-    
+    //构造函数
+    MyBinTreeNode():
+    parent(NULL),lc(NULL),rc(NULL),height(0),npl(1),color(RB_RED){}
+    MyBinTreeNode(T e,MyBinTreeNodePosi(T) p=NULL,MyBinTreeNodePosi(T) lc=NULL,MyBinTreeNodePosi(T) rc=NULL,int h=0,int l=1,RBColor c=RB_RED ):
+    data(e),parent(p),lc(lc),rc(rc),height(h),npl(l),color(c){}
+    //操作接口
+    ///统计当前节点后代总数，即以当前节点为根的树的规模
+    int size();
+    //作为当前节点的左孩子插入新节点
+    MyBinTreeNodePosi(T) insertAsLc(T const&);
+    //作为当前节点的右孩子插入新节点
+    MyBinTreeNodePosi(T) insertAsRc(T const&);
+    //取当前节点的下一个,中序遍历序列的后一个元素
+    MyBinTreeNodePosi(T) next();
+    //子树层次遍历
+    template <typename VST> void travLevel(VST&);
+    //子树先序遍历
+    template <typename VST> void travPre(VST&);
+    //子树中序遍历
+    template <typename VST> void travIn(VST&);
+    //子树后序遍历
+    template <typename VST> void travPost(VST&);
+    //重载小于操作符
+    bool operator<(MyBinTreeNode const& bn){return data<bn.data;}
+    //重载==操作符
+    bool operator== (MyBinTreeNode const& bn){return data==bn.data;}
 
 };
+
+template <typename T> MyBinTreeNodePosi(T) MyBinTreeNode<T>::insertAsLc(const T & e){
+    return lc=new MyBinTreeNode(e,this);
+}
+template <typename T> MyBinTreeNodePosi(T) MyBinTreeNode<T>::insertAsRc(const T &e){
+    return rc=new MyBinTreeNode(e,this);
+}
